a0215d1be182d6f6be0ebc8a6e53290870d13b66
Aminion <aminion@protonmail.com>
2023-12-10 21:31:05 +0200

    init libs

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+#![feature(default_free_fn)]
+
+extern crate crossbeam;
+extern crate glam;
+extern crate legion;
+extern crate rand;
+extern crate sdl2;
+
+use sdl2::mixer::InitFlag;
+use sdl2::pixels::Color;
+use sdl2::render::WindowCanvas;
+use sdl2::EventPump;
+
+fn initialize<'a, 'b>() -> Result<(WindowCanvas, EventPump), String> {
+    // Initialize libraries
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let _audio = sdl_context.audio()?;
+
+    let _mixer_context =
+        sdl2::mixer::init(InitFlag::MP3 | InitFlag::FLAC | InitFlag::MOD | InitFlag::OGG)?;
+
+    // Number of mixing channels available for sound effect `Chunk`s to play
+    // simultaneously.
+    sdl2::mixer::allocate_channels(20);
+
+    // Initialize window systems
+    let window = video_subsystem
+        .window("KSP 2D", 1280, 720)
+        .position_centered()
+        .build()
+        .expect("could not initialize video subsystem");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("could not make a canvas");
+
+    // Render first empty frame
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let event_pump = sdl_context.event_pump()?;
+
+    Ok((canvas, event_pump))
+}
+
+pub fn main() -> () {
+    let _ = initialize().unwrap();
+    loop {}
+}
