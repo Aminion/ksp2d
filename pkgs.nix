1f459bfcc4c94f5e54020ad7cc7cfab466ddfa78
Aminion <aminion@protonmail.com>
2024-01-25 23:12:20 +0200

    upd  pkgs

--- pkgs.nix
+++ pkgs.nix
@@ -2,6 +2,6 @@
 import ((import <nixpkgs> {}).fetchFromGitHub {
   owner = "NixOS";
   repo = "nixpkgs";
-  rev = "3ab933ca57b1d06cf1991320c922ee2a8405b77a";
-  sha256  = "pf+dYMVo8E1O7ZNcOInEiD1vfoNrGlLrG/Z3OtwYTQ8=";
+  rev = "f67b36bad67f1cf683a61cf6ac98e12d3b5a6532";
+  sha256  = "qLRT7SxojcBPc7Dzocd6Www4+I+UeFrkcmR1Gxgz/vM=";
 })
