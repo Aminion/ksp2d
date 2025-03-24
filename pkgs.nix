913151b185e23a6a677db8648266e542bc9b46c9
Aminion <aminion@protonmail.com>
2025-03-24 14:21:35 +0200

    simplify env

--- pkgs.nix
+++
@@ -1,7 +0,0 @@
-# To update nix-prefetch-git https://github.com/NixOS/nixpkgs
-import ((import <nixpkgs> {}).fetchFromGitHub {
-  owner = "NixOS";
-  repo = "nixpkgs";
-  rev = "87f0efcadc6089cbfd82992dd78d42f3ec2750fb";
-  sha256  = "sha256-zPMo4a+e2P4cVBd7MdUCUrRmY2LD86Vj3J4PrcoBr/Y=";
-})
--- shell.nix
+++ shell.nix
@@ -1,18 +1,26 @@
-with import ./pkgs.nix {};
+let
+  pkgs = import (fetchTarball("channel:nixpkgs-unstable")) {};
 
-stdenv.mkDerivation rec {
+in pkgs.stdenv.mkDerivation rec {
   name = "ksp2d-rust-env";
-  env = buildEnv { name = name; paths = buildInputs; };
+  env = pkgs.buildEnv { name = name; paths = buildInputs; };
 
-  buildInputs = [
+  buildInputs = with pkgs; [
     llvm_19
+    cargo
+    rustc
+    rustfmt
+    rust-analyzer
+    clippy
     SDL2
     SDL2_mixer
     SDL2_gfx
     SDL2_ttf
     open-sans
   ];
+
+   
    shellHook = ''
-    ln -sf ${open-sans}/share/fonts/truetype ./
+    ln -sf ${pkgs.open-sans}/share/fonts/truetype ./
   '';
 }
