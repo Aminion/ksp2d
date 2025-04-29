613dfa7b875aa9b388186188e7df6e688a544eb7
Aminion <aminion@protonmail.com>
2025-04-29 22:02:16 +0300

    new generation

---
+++ src/fonts.rs
@@ -0,0 +1,58 @@
+use fontdue::{
+    layout::{Layout, TextStyle},
+    Font, FontSettings,
+};
+use fontdue_sdl2::FontTexture;
+use glam::Vec2;
+use sdl2::pixels::Color;
+
+use crate::CanvasResources;
+
+pub struct FontRenderer<const N: usize> {
+    pub font: [Font; N],
+    pub layout: Layout<Color>,
+}
+
+impl<const N: usize> FontRenderer<N> {
+    pub fn new(fonts: [Font; N]) -> Result<Self, String> {
+        let layout = Layout::new(fontdue::layout::CoordinateSystem::PositiveYDown);
+        Ok(FontRenderer {
+            font: fonts,
+            layout,
+        })
+    }
+
+    pub fn render_text(
+        &mut self,
+        canvas_resources: &mut CanvasResources,
+        text: &str,
+        at: Vec2,
+        font_size: f32,
+        font_color: Color,
+        font_index: usize,
+    ) -> Result<(), String> {
+        self.layout.reset(&fontdue::layout::LayoutSettings {
+            x: at.x,
+            y: at.y,
+            ..Default::default()
+        });
+        self.layout.append(
+            &self.font,
+            &TextStyle::with_user_data(text, font_size, font_index, font_color),
+        );
+        let mut texture = FontTexture::new(&canvas_resources.texture_creator)?;
+        texture.draw_text(
+            &mut canvas_resources.canvas,
+            &self.font,
+            self.layout.glyphs(),
+        )?;
+
+        Ok(())
+    }
+}
+
+pub fn load_fonts() -> [Font; 1] {
+    let font_data = include_bytes!("../../../truetype/OpenSans-Regular.ttf") as &[u8];
+    let font = Font::from_bytes(font_data, FontSettings::default()).expect("Failed to load font");
+    [font]
+}
---
+++ src/system_generation.rs
@@ -0,0 +1,85 @@
+use std::f64::consts::PI;
+
+use glam::{dvec2, DVec2};
+use rand::Rng;
+use sdl2::pixels::Color;
+
+use crate::ksp2d::components::{
+    celestial_body::{CelestialBody, CelestialBodyType},
+    newton_body::NewtonBody,
+};
+
+pub fn get_system(system_radius: f64) -> Vec<(CelestialBody, NewtonBody)> {
+    let system_center = dvec2(system_radius, system_radius);
+
+    let planet_range = 0.01..=0.04;
+    let planet_density_range = 1330.0..=5420.0;
+    let planet_mass_range = 0.330e24..=1898.6e24;
+    let interval_range = 0.05..=0.1;
+
+    let star_radius = 6.957e8 / 8.0;
+    let star_mass = 1.988416e30;
+
+    let star = (
+        CelestialBody {
+            b_type: CelestialBodyType::Star,
+            color: Color::YELLOW,
+            radius: star_radius,
+        },
+        NewtonBody {
+            angle: DVec2::Y,
+            angular_vel: 1.0,
+            mass: star_mass,
+            pos: system_center,
+            vel: DVec2::ZERO,
+            acc: DVec2::ZERO,
+        },
+    );
+    let mut system: Vec<(CelestialBody, NewtonBody)> = Vec::new();
+    system.push(star);
+
+    let mut rng = rand::rng();
+    let mut cursor = star_radius;
+    let limit =
+        system_radius - (system_radius * planet_range.end() + system_radius * interval_range.end());
+    while cursor <= limit {
+        let interval = system_radius * rng.random_range(interval_range.clone());
+        cursor += interval;
+        let mass = rng.random_range(planet_mass_range.clone());
+        let dencity = rng.random_range(planet_density_range.clone());
+        let p_r = calculate_radius(mass, dencity);
+        cursor += p_r;
+        let angle = rng.random_range(0.0..2.0 * std::f64::consts::PI);
+        let position = cursor * DVec2::from_angle(angle);
+        let orbital_speed =
+            (physical_constants::NEWTONIAN_CONSTANT_OF_GRAVITATION * star.1.mass / cursor).sqrt();
+
+        let velocity = DVec2::new(-position.y, position.x).normalize() * orbital_speed;
+
+        let planet = (
+            CelestialBody {
+                b_type: CelestialBodyType::Planet,
+                color: Color::GREEN,
+                radius: p_r,
+            },
+            NewtonBody {
+                angle: DVec2::Y,
+                angular_vel: angle,
+                mass: mass,
+                pos: system_center + position,
+                vel: velocity,
+                acc: DVec2::ZERO,
+            },
+        );
+        system.push(planet);
+        cursor += p_r;
+    }
+    system
+}
+
+fn calculate_radius(mass: f64, density: f64) -> f64 {
+    let volume = mass / density;
+    let radius_cubed = (3.0 * volume) / (4.0 * PI);
+    let radius = radius_cubed.cbrt();
+    radius
+}
